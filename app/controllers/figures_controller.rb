8768504e5432ee65046e11a38649d447abd45d17
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-13 18:13:09 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -8,7 +8,8 @@ class FiguresController < ApplicationController
     erb :'figures/new'
   end
 
-  get '/figures/:id' do 
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
     erb :'figures/show'
   end
 
