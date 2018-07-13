96d9db98f40dd6fd4e4478ead490e7713b3562eb
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-13 19:30:34 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -3,4 +3,9 @@ class LandmarksController < ApplicationController
     @landmarks = Landmark.all
     erb :'landmarks/index'
   end
+
+  get '/landmarks/new' do
+
+    erb :'landmarks/new'
+  end
 end
