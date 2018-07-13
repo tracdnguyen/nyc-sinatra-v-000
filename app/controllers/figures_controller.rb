e989d9956d821fd2f2770fee0f52aca907bfbf79
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-13 16:44:13 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -10,11 +10,11 @@ class FiguresController < ApplicationController
   post '/figures' do
     @figure = Figure.create(params["figure"])
     if !params[:landmark][:name].empty?
-      @figure.landmarks << Landmark.create(params["landmark"]["name"])
+      @figure.landmarks << Landmark.create(params["new_landmark"])
     end
 
     if !params[:title][:name].empty?
-      @figure.titles << Title.create(params["title"]["name"])
+      @figure.titles << Title.create(params["new_title"])
     end
 
     @figure.save
