5bfca75380fb671050b63a2a468fbf571dfc19da
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-13 20:01:45 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -12,4 +12,14 @@ class LandmarksController < ApplicationController
     Landmark.create(:name => params["landmark"]["name"], :year_completed => params["landmark"]["year_completed"])
     redirect to "/landmarks"
   end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/show"
+  end
+  
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/edit"
+  end
 end
