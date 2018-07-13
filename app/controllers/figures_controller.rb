df540e9a505ccb6aa6218d4b04083815e74ebe8c
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-13 19:27:03 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -49,5 +49,20 @@ class FiguresController < ApplicationController
     erb :'figures/edit'
   end
 
+  post '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
 
 end
