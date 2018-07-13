c519177848a57021d2ed8086416400922762632f
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-13 19:52:23 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -7,4 +7,9 @@ class LandmarksController < ApplicationController
   get '/landmarks/new' do
     erb :'landmarks/new'
   end
+
+  post '/landmarks' do 
+    @landmark = Landmark.create(name: params[:landmark][:name], year_completed: [:landmark][:year_completed])
+    redirect to "/landmarks/#{@landmark.id}"
+  end 
 end
