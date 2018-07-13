96c4b53fc3777e561986b8e39a3ee81d50384698
Trac Nguyen <nguyendtrac@gmail.com>
2018-07-13 17:40:29 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -8,39 +8,30 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    @title_ids = []
-    if !params[:title][:name].empty?
-      @titles << Title.create(name: params["title"]["name"])
-    end
-    if !params[:figure][:title_ids].empty?
-      params[:figure][:title_ids].each do |title|
-        id = landmark.gsub("title_","").to_i
-        @title_ids << Title.find(id)
-      end
-    end
-    if !params[:title][:name].empty?
-      @figure.titles << Title.create(name: params["title"]["name"])
-    end
-
-    @landmark_ids = []
+    @landmarks = []
     if !params[:landmark][:name].empty?
-      @landmarks << Landmark.create(name: params["landmark"]["name"])
+      @landmarks << Landmark.create(name: params[:landmark][:name])
     end
-    if !params[:figure][:landmark_ids].empty?
+    if params[:figure][:landmark_ids] != nil
       params[:figure][:landmark_ids].each do |landmark|
         id = landmark.gsub("landmark_","").to_i
-        @landmark_ids << Landmark.find(id)
+        @landmarks << Landmark.find(id)
       end
     end
+    @titles = []
     if !params[:title][:name].empty?
-      @titles << Title.create(name: params["title"]["name"])
+      @titles << Title.create(params[:title])
     end
-
+    if params[:figure][:title_ids] != nil
+      params[:figure][:title_ids].each do |title|
+        id = title.gsub("title_","").to_i
+        @titles << Title.find(id)
+      end
+    end
     @figure = Figure.find_by(name: params[:figure][:name]) || Figure.create(name: "#{params[:figure][:name]}")
-    @figure.landmarks = @landmark_ids
-    @figure.titles = @title_ids
-    @figure.save
-
+    @figure.landmarks = @landmarks
+    @figure.titles = @titles
+    
     redirect to "/figures/#{@figure.id}"
   end
 
